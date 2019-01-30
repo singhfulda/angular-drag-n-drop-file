422ca22c461b22c635d1cd34e3e709dec4af3dd6
hs <hs@diamos.com>
2019-01-30 09:00:28 +0100

    component created with ng g c

---
+++ DragNDropApp/src/app/drag-drop/drag-drop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drag-drop',
+  templateUrl: './drag-drop.component.html',
+  styleUrls: ['./drag-drop.component.less']
+})
+export class DragDropComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
